04ffc9dee57a7cc57322f8c1b6a2e8aec88d147a
Rishav Das <rishavdas27602@gmail.com>
2024-09-22 16:37:54 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         // Task 3: Create an object of class B and call meth method
+        A obj1 = new A();
         B obj = new B();
 
         // Task 3: Call meth method of class A (before overriding)
-        System.out.println(obj.meth());  // This will now invoke the overridden method in class B
+        System.out.println(obj1.meth());  // This will now invoke the overridden method in class B
 
         // Task 5: Call the overridden meth method from the object of class B
         System.out.println(obj.meth());  // This will print the overridden method's output
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,29 +1,9 @@
-//package com.driver.test;
-//
-//import static org.junit.Assert.*;
-//import org.junit.jupiter.api.Test;
-//
-//public class TestCases {
-//
-//}
 package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-import com.driver.A;
-import com.driver.B;
 
 public class TestCases {
 
-    @Test
-    public void testMethInClassA() {
-        A a = new A();
-        assertEquals("Invoking method from class A", a.meth());
-    }
-
-    @Test
-    public void testMethInClassB() {
-        B b = new B();
-        assertEquals("Method is overridden in Extended class B", b.meth());
-    }
 }
+
