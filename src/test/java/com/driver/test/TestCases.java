63575f4c3af3af632b00834ebd9b735ec8b7d0b5
Rishav Das <rishavdas27602@gmail.com>
2024-09-28 12:58:41 +0530

    testcases solved

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,9 +1,17 @@
 package com.driver.test;
 
 import static org.junit.Assert.*;
+
+import com.driver.Main;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
 
+    @Test
+    public void testprintHelloWorld2() {
+        Main.B obj = new Main.B();
+        assertEquals("Method is overridden in Extended class B", obj.meth());
+    }
 }
+
 
