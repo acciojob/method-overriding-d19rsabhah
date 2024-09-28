ccb0356257d640ade0a08a66e7f16525b7630fde
Rishav Das <rishavdas27602@gmail.com>
2024-09-28 13:14:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
         B obj = new B();
-
+        
         System.out.println(obj.meth());  // This will print the overridden method's output
     }
 }
